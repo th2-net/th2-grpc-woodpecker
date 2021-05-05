6cee7dcf9901799292bbddeeefa25d467ae8e3be
nikita.smirnov <nikita.smirnov@exactprosystems.com>
2021-05-05 10:05:30 +0300

    [th2-1515] Migrated to sonatype artifacts

--- setup.py
+++ setup.py
@@ -115,7 +115,7 @@ setup(
     license='Apache License 2.0',
     python_requires='>=3.7',
     install_requires=[
-        'th2-grpc-common~=3.1.1'
+        'th2-grpc-common~=3.1.2'
     ],
     packages=packages,
     package_data=package_data,
